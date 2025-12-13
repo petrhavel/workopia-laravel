82872c876bf580832aa050bdb60a643d84326de5
Petr Havel <petrhavel.info@gmail.com>
2025-12-13 11:19:09 +0100

    final2

--- app/Http/Controllers/ApplicantController.php
+++ app/Http/Controllers/ApplicantController.php
@@ -46,7 +46,7 @@ class ApplicantController extends Controller
         $application->save();
 
         // Send email to owner
-        Mail::to($job->user->email)->send(new JobApplied($application, $job));
+        // Mail::to($job->user->email)->send(new JobApplied($application, $job));
 
         return redirect()->back()->with('success', 'Your application has been submitted');
     }
